10e0925a37f2bfe0ef86280f594e091fab61a538
soehina <kamuhina@gmail.com>
2019-11-15 23:21:42 +0900

    controllerの変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,11 +3,5 @@ class TopController < ApplicationController
         agent = Mechanize.new
         page = agent.get("https://nemlog.nem.social/")
         @elements = page.search('.visit-w')
-        # elements.each do |element|
-        #     ele_title=element.inner_text
-        #     ele_url=element.get_attribute(:href)
-        #     @elements=Article.create(url: params[:ele_url],title: params[:ele_title])
-        # end
-        # @elements=Article.all
     end
 end
